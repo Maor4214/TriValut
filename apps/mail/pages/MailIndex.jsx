7663cba5cb5622828675ee8e017fbc8ec84d030a
Maor <Maoryad@gmail.com>
2025-02-21 23:48:38 +0200

    Add a start for mail serivce

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,36 @@
+import { mailService } from '../services/mail.service.js'
+
+const { useEffect, useState } = React
+const { useSearchParams } = ReactRouterDOM
 
 export function MailIndex() {
-    return <section className="container">Mail app</section>
-}
+  const [notes, setNotes] = useState(null)
 
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [filterBy, setFilterBy] = useState(
+    mailService.getFilterFromSearchParams(searchParams)
+  )
+
+  //   useEffect(() => {
+  //     setSearchParams(filterBy)
+  //     loadNotes()
+  //   }, [filterBy, notes])
+
+  //   function loadNotes() {
+  //     mailService.query(filterBy).then((notes) => {
+  //       setNotes(notes)
+  //       console.log('notes', notes)
+  //     })
+  //   }
+
+  //   function onRemoveNote(noteId) {
+  //     mailService.remove(noteId).then(() => {
+  //       loadNotes()
+  //     })
+  //   }
+  return (
+    <section className="mail-container">
+      <div>testing to see maor didnt break anything</div>
+    </section>
+  )
+}
