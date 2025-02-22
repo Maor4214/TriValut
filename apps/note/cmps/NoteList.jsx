ac3f3bb0337a04eed9e1f1ff86feb0e7d126c211
Tomer <tomera.almog9@gmail.com>
2025-02-22 16:51:17 +0200

    put note list in columns

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,30 +1,31 @@
-import { NotePreview } from "./NotePreview.jsx"
+import { NotePreview } from './NotePreview.jsx'
 const { useEffect, useState } = React
-const { Link, Outlet,useSearchParams,useNavigate } = ReactRouterDOM
+const { Link, Outlet, useSearchParams, useNavigate } = ReactRouterDOM
 
-export function NoteList({notes, onRemoveNote}) {
- 
-    const navigate = useNavigate()
+export function NoteList({ notes, onRemoveNote }) {
+  const navigate = useNavigate()
 
-    function onNoteClick(noteId){
-        navigate(`/notes/${noteId}`)
-    }
+  function onNoteClick(noteId) {
+    navigate(`/notes/${noteId}`)
+  }
 
-    if(!notes) return <div>Loading...</div>
-    return  <section className="note-list">
-        
-            <nav>
-                <Link replace to="/notes/1">edit note</Link>
-            </nav>
-
-
-            <Outlet />
-        { notes && notes.map(note => { 
-            return <div  key={note.id}>
-                <NotePreview className="note-preview" note={note} onNoteClick={onNoteClick}/>      
-                <button onClick={() => onRemoveNote(note.id)}>X</button>     
+  if (!notes) return <div>Loading...</div>
+  return (
+    <section className="note-list">
+      <Outlet />
+      {notes &&
+        notes.map((note) => {
+          return (
+            <div key={note.id}>
+              <NotePreview
+                className="note-preview"
+                note={note}
+                onNoteClick={onNoteClick}
+              />
+              <button onClick={() => onRemoveNote(note.id)}>X</button>
             </div>
+          )
         })}
-
     </section>
+  )
 }
