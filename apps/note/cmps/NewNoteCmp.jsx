8e4b2df78c6f5c25179ddf255539a48b26fc56b8
Tomer <tomera.almog9@gmail.com>
2025-02-24 22:44:37 +0200

    Add new note cmp - initial styling -

---
+++ apps/note/cmps/NewNoteCmp.jsx
@@ -0,0 +1,14 @@
+export function NewNoteCmp() {
+  return (
+    <section className="new-note-container">
+      <form className="new-note-form">
+        <input type="text" placeholder="Take a note..." />
+        <div className="new-note-btns">
+          <button>X</button>
+          <button>X</button>
+          <button>X</button>
+        </div>
+      </form>
+    </section>
+  )
+}
